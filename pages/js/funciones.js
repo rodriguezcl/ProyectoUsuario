03e021e19801b70e7abb61bed9aed45bc65551c3
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-05-17 20:15:29 -0300

    Interfaz inicio terminada

--- pages/js/funciones.js
+++ pages/js/funciones.js
@@ -1,53 +1,66 @@
-function getAll()
-{
- 
- fetch("http://localhost:58112/api/Usuario")
- .then( (response) => response.json())
- .then ( (data) => {
+function getAll() {
+  
+  fetch("http://localhost:58112/api/Usuario")
+    .then((response) => response.json())
+    .then((data) => {
+      const _tbody = document.getElementById("getAll");
+      data.forEach((o) => {
+        let _tr = `<tr>
+        <td>${o.id} </td>
+        <td>${o.Nombre} </td>
+        <td>${o.Apellido} </td>
+        <td>${o.Direccion} </td>
+        <td>${o.Telefono} </td>   
+        </tr>`;
 
-  console.log(data)
+        _tbody.innerHTML += _tr;
+      });
+    });
+}
+getAll();
 
- })   
+
+function getId(id) {
+    _txtID = document.getElementById("txtID");
+    id = _txtID.value;
+    
+  fetch("http://localhost:58112/api/Usuario/" + id)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const _tbody = document.getElementById("getId");
+      let _tr = `<tr>
+      <td>${data.id} </td>
+      <td>${data.Nombre} </td>
+      <td>${data.Apellido} </td>
+      <td>${data.Direccion} </td>
+      <td>${data.Telefono} </td>   
+      </tr>`;
+
+      _tbody.innerHTML += _tr;
+
+
+      _txtID.value = "";
+      _txtID.focus();
+    });
 }
 
-function getId(id)
-{
-
-
-    fetch("http://localhost:58112/api/Usuario" + id)
-    .then( (response) => response.json())
-    .then ( (data) => {
-
-      console.log(data)
-
- })
+function post(obj) {
+  fetch("http://localhost:58112/api/Usuario", {
+    method: "Post",
+    body: JSON.stringify(obj),
+  });
 }
 
-function post(obj)
-{
-
-    fetch("http://localhost:58112/api/Usuario", {
-
-        method: "Post",
-        body: JSON.stringify( obj)
-
-    })
+function Eliminar(id) {
+  fetch("http://localhost:58112/api/Usuario" + id, {
+    method: "Delete",
+  });
 }
 
-function Eliminar(id)
-{
-    fetch("http://localhost:58112/api/Usuario" + id, {
-      method: "Delete"
-})
-}
-
-
-function Put(id, obj)
-{
-
-    fetch("http://localhost:58112/api/Usuario" + id, {
-     method: "Put",
-     body: JSON.stringify( obj)
-
-})
+function Put(id, obj) {
+  fetch("http://localhost:58112/api/Usuario" + id, {
+    method: "Put",
+    body: JSON.stringify(obj),
+  });
 }
