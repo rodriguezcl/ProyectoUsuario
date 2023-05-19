22ef229c3ebeb0d1f089f2b05301ae5bc209f1af
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-05-19 18:09:08 -0300

    funciones.js OK

--- pages/js/funciones.js
+++ pages/js/funciones.js
@@ -1,5 +1,6 @@
+//FUNCION GET 
+
 function getAll() {
-  
   fetch("http://localhost:58112/api/Usuario")
     .then((response) => response.json())
     .then((data) => {
@@ -18,11 +19,12 @@ function getAll() {
     });
 }
 
+//FUNCION GET BY ID
 
 function getId(id) {
-    _txtID = document.getElementById("txtID");
-    id = _txtID.value;
-    
+  const _txtID = document.getElementById("txtID");
+  id = _txtID.value;
+
   fetch("http://localhost:58112/api/Usuario/" + id)
     .then((response) => response.json())
     .then((data) => {
@@ -37,8 +39,105 @@ function getId(id) {
 
       _tbody.innerHTML += _tr;
 
-
       _txtID.value = "";
       _txtID.focus();
     });
+}
+
+//FUNCION PUT
+
+let obj = "";
+
+function put(obj) {
+  const _txtIDPut = document.getElementById("txtIDPut");
+  const _txtNombrePut = document.getElementById("txtNombrePut");
+  const _txtApellidoPut = document.getElementById("txtApellidoPut");
+  const _txtDireccionPut = document.getElementById("txtDireccionPut");
+  const _txtTelefonoPut = document.getElementById("txtTelefonoPut");
+
+  obj = {
+    id: _txtIDPut.value,
+    Nombre: _txtNombrePut.value,
+    Apellido: _txtApellidoPut.value,
+    Direccion: _txtDireccionPut.value,
+    Telefono: _txtTelefonoPut.value,
+  };
+  $.ajax({
+    type: "PUT",
+    dataType: "json",
+    url: "http://localhost:58112/api/Usuario/" + obj.id,
+    data: obj,
+    success: function (data) {
+      alert("PUT OK!");
+      _txtIDPut.value = "";
+      _txtNombrePut.value = "";
+      _txtApellidoPut.value = "";
+      _txtDireccionPut.value = "";
+      _txtTelefonoPut.value = "";
+      _txtIDPut.focus();
+    },
+    error: function (error) {
+      alert(error);
+    },
+  });
+}
+
+//FUNCION POST
+
+function post(obj) {
+  const _txtNombrePost = document.getElementById("txtNombrePost");
+  const _txtApellidoPost = document.getElementById("txtApellidoPost");
+  const _txtDireccionPost = document.getElementById("txtDireccionPost");
+  const _txtTelefonoPost = document.getElementById("txtTelefonoPost");
+
+  obj = {
+    Nombre: _txtNombrePost.value,
+    Apellido: _txtApellidoPost.value,
+    Direccion: _txtDireccionPost.value,
+    Telefono: _txtTelefonoPost.value,
+  };
+  $.ajax({
+    type: "POST",
+    dataType: "json",
+    url: "http://localhost:58112/api/Usuario",
+    data: obj,
+    success: function (data) {
+      alert("POST OK!");
+      _txtNombrePost.value = "";
+      _txtApellidoPost.value = "";
+      _txtDireccionPost.value = "";
+      _txtTelefonoPost.value = "";
+      _txtNombrePost.focus();
+    },
+    error: function (error) {
+      alert(error);
+    },
+  });
+}
+
+//FUNCION DELETE
+
+let idEliminar = "";
+
+function eliminar(idEliminar) {
+  const _txtIDEliminar = document.getElementById("txtIDEliminar");
+
+  idEliminar = {
+    id: _txtIDEliminar.value,
+  };
+
+  $.ajax({
+    type: "DELETE",
+    dataType: "json",
+    url: "http://localhost:58112/api/Usuario/" + idEliminar.id,
+    data: idEliminar,
+    success: function (data) {
+      alert("DELETE OK!");
+      _txtIDEliminar.value = "";
+      _txtIDEliminar.focus();
+    },
+    error: function (error) {
+      alert(error);
+    },
+  });
 }
