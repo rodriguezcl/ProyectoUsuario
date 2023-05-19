63373cbc3a80ff03c5885518a507a22005210d9e
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-05-19 17:24:09 -0300

    Limpieza codigo

--- pages/js/funciones.js
+++ pages/js/funciones.js
@@ -42,23 +42,3 @@ function getId(id) {
       _txtID.focus();
     });
 }
-
-// function post(obj) {
-//   fetch("http://localhost:58112/api/Usuario", {
-//     method: "Post",
-//     body: JSON.stringify(obj),
-//   });
-// }
-
-// function Eliminar(id) {
-//   fetch("http://localhost:58112/api/Usuario" + id, {
-//     method: "Delete",
-//   });
-// }
-
-// function Put(id, obj) {
-//   fetch("http://localhost:58112/api/Usuario" + id, {
-//     method: "Put",
-//     body: JSON.stringify(obj),
-//   });
-// }
